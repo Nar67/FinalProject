a45fe07736e5e6042f527e841224f0299dc6ab0a
Nar67 <narcis.tg@gmail.com>
2017-07-08 12:04:04 +0200

    Added navigation drawer with examples (not working)

--- app/src/main/java/com/tg/narcis/finalproject/fragments/Memory.java
+++ app/src/main/java/com/tg/narcis/finalproject/fragments/Memory.java
@@ -6,13 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 
 import com.tg.narcis.finalproject.R;
 
 public class Memory extends Fragment {
 
     View rootview;
-
+    GridLayout grid;
     public Memory() {
         // Required empty public constructor
     }
@@ -23,7 +24,7 @@ public class Memory extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootview = inflater.inflate(R.layout.fragment_memory, container, false);
-
+        grid = (GridLayout) rootview.findViewById(R.id.grid);
 
 
         return rootview;
